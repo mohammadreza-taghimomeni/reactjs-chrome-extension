570ba06b213f8621b0dabbe482f73ccdfff0d77e
Mohammad Reza Taghimomeni <mrm.tm95@gmail.com>
2023-08-30 14:53:44 +0330

    init design of site

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Typography } from "antd";
 import "./popup.css";
+import { UilSearch } from "@iconscout/react-unicons";
 
+const { Text, Title } = Typography;
 const Popup = () => {
   return (
     <div>
+      <Title level={4}>
+        <UilSearch />
+        <br />
+        Where Are You?
+      </Title>
+      <Text type="secondary">
+        Select the place of extraction of job seekers
+      </Text>
+
       <h1 className="text-4xl text-green-500">Hello World</h1>
 
       <Button type="primary">Button</Button>
