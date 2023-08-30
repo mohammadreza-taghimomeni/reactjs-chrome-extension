5fa61e45dbea6843d577dce8e53daf6444d85de0
Mohammad Reza Taghimomeni <mrm.tm95@gmail.com>
2023-08-30 14:43:00 +0330

    add antd

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import '../assets/tailwind.css'
 import Popup from "./popup";
+import { ConfigProvider } from "antd";
 
 function init() {
-    const appContainer = document.createElement('div')
-    document.body.appendChild(appContainer)
-    if (!appContainer) {
-        throw new Error("Can not find AppContainer");
-    }
-    const root = createRoot(appContainer)
-    console.log(appContainer)
-    root.render(<Popup />);
+  const appContainer = document.createElement("div");
+  document.body.appendChild(appContainer);
+  if (!appContainer) {
+    throw new Error("Can not find AppContainer");
+  }
+  const root = createRoot(appContainer);
+  console.log(appContainer);
+
+  root.render(
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#1d39c4",
+          fontSize: 14,
+          fontWeightStrong: 400,
+          fontFamily: "Roboto",
+        },
+      }}
+      direction={"ltr"}
+    >
+      <Popup />
+    </ConfigProvider>
+  );
 }
 
 init();
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import './popup.css'
+import { Button } from "antd";
+import "./popup.css";
 
 const Popup = () => {
-    return (
-        <div>
-            <h1 className="text-4xl text-green-500">Hello World</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-4xl text-green-500">Hello World</h1>
+
+      <Button type="primary">Button</Button>
+    </div>
+  );
 };
 
 export default Popup;
